69ca1ddec69935a0ccaa3a726fd76a6476cfef48
Muhammad Yaqdhan Taqy Ariana <59348660+dhan14@users.noreply.github.com>
2020-03-04 23:27:58 +0700

    Muhammad Yaqdhan T.A - Login dengan function

---
+++ Python/Quest/Quest2.py
@@ -0,0 +1,11 @@
+id_user = 'Dhan'
+passw = "123"
+
+
+input_akun = input("Masukan ID : ")
+input_pass = input("Masukan Pass : ")
+
+if(input_akun==id_user and input_pass==passw):
+    print ("Anda Masuk")
+else:
+    print ("Id atau password salah")
